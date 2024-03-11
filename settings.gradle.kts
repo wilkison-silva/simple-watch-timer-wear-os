40eef32b6a3d3ddb59023dcb74c3dfb2707eac3d
Wilkison <wilkisonmartinsdasilva@gmail.com>
2024-03-11 07:24:35 -0400

    Cria projeto

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Simple Watch Timer Wear OS"
+include(":app")
+ 
